4746b50359b9516014ba23823fe731aca164d7e5
AjayManinder <102263680+AjayManinder@users.noreply.github.com>
2022-11-04 09:59:09 -0500

    view

--- src/App.js
+++ src/App.js
@@ -1,12 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Search from "./Search";
+import Results from "./Results";
+import Detail from "./Detail";
+import "./App.css";
 
-const App = () => {
-  return(
-<div>
-  <h1>hello world rj</h1>
-  <h5>hi</h5>
-</div>
-  );
+function App() {
+const [state, setState] = useState({
+	s: "sherlock",
+	results: [],
+	selected: {},
+});
+
+const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=383903a0";
+
+const searchInput = (e) => {
+	let s = e.target.value;
+
+	setState((prevState) => {
+	return { ...prevState, s: s };
+	});
+};
+
+const search = (e) => {
+	if (e.key === "Enter") {
+	axios(apiurl + "&s=" + state.s).then(({ data }) => {
+		let results = data.Search;
+
+		console.log(results);
+
+		setState((prevState) => {
+		return { ...prevState, results: results };
+		});
+	});
+	}
+};
+
+const openDetail = (id) => {
+	axios(apiurl + "&i=" + id).then(({ data }) => {
+	let result = data;
+
+	setState((prevState) => {
+		return { ...prevState, selected: result };
+	});
+	});
+};
+
+const closeDetail = () => {
+	setState((prevState) => {
+	return { ...prevState, selected: {} };
+	});
+};
+
+return (
+	<div className="App">
+	<header className="App-header">
+		<h1>Search any word for movie suggestion</h1>
+	</header>
+	<main>
+		<Search searchInput={searchInput} search={search} />
+
+		<Results results={state.results} openDetail={openDetail} />
+
+		{typeof state.selected.Title != "undefined" ? (
+		<Detail selected={state.selected} closeDetail={closeDetail} />
+		) : (
+		false
+		)}
+	</main>
+	</div>
+);
 }
 
 export default App;
